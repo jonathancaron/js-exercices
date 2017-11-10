b5ceb3c90c5ea61e522de7caf993a55562d94757
jonathancaron <jonathan.caron04@gmail.com>
2017-11-10 09:58:30 +0100

    Exercice 4

--- js/my.js
+++ js/my.js
@@ -1,5 +1,11 @@
-let nom = "Caron";
+/*let nom = "Caron";
 let prenom = "Jonathan";
 let ville = "Rhode-Saint-Genèse";
 
-alert("Bonjour " + prenom)
+alert("Bonjour " + prenom);*/
+
+let nom = prompt("Quel est votre nom ?");
+let prenom = prompt("Quel est votre prenom ?");
+let ville = prompt("Quel est votre ville ?");
+
+alert("Nom: " + nom + ", prénom: " + prenom + ", ville: " + ville)
