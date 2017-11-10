2d9a8e973a1a6ffab34b12080bf98c3d6e5b2158
jonathancaron <jonathan.caron04@gmail.com>
2017-11-10 10:42:26 +0100

    Exercice 8 - FINALISATION

--- js/my.js
+++ js/my.js
@@ -1,18 +1,8 @@
-alert("Pour votre inscription, nous avons besoin de divers informations (la suite à la prochaine box)");
+let age = prompt("Quel est votre âge ?");
 
-
-let pointure = prompt("Quel est votre pointure ? (cm)");
-let anneenaissance = prompt("Quel est votre année de naissance ?");
-
-function calcul(pointure, anneenaissance){
-  let un = pointure*2;
-  let deux = un + 5;
-  let trois = deux * 50;
-  let quatre = trois - anneenaissance;
-  let cinq = quatre + 1766;
-
-  return cinq;
+if(age > 18){
+  alert("Vous avez plus de 18 ans.");
 }
-
-let resultat = calcul(pointure,anneenaissance);
-alert(resultat);
+else{
+  alert("Vous êtes mineur");
+}
