fe5d2423c1230c20ec13b8316564ac8fc6069f13
jonathancaron <jonathan.caron04@gmail.com>
2017-11-10 10:13:00 +0100

    Exercice 5

--- js/my.js
+++ js/my.js
@@ -1,11 +1,12 @@
-/*let nom = "Caron";
-let prenom = "Jonathan";
-let ville = "Rhode-Saint-Genèse";
+alert("Dans les prochaines boites de dialogue, merci d'inscrire deux nombres à virgule");
+let nb1 = prompt("Premier nombre à virgule");
+let nb2 = prompt("Deuxième nombre à virgule");
+let nombre1 = parseFloat(nb1);
+let nombre2 = parseFloat(nb2);
 
-alert("Bonjour " + prenom);*/
+function multiplication (a,b){
+  let result = nombre1*nombre2;
+  return result;
+};
 
-let nom = prompt("Quel est votre nom ?");
-let prenom = prompt("Quel est votre prenom ?");
-let ville = prompt("Quel est votre ville ?");
-
-alert("Nom: " + nom + ", prénom: " + prenom + ", ville: " + ville)
+alert(multiplication(nombre1,nombre2));
