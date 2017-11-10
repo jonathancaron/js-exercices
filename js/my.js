684270a920378358f1e5ecb3a1680046c79bf9fc
jonathancaron <jonathan.caron04@gmail.com>
2017-11-10 11:27:31 +0100

    base2 - ex1

--- js/my.js
+++ js/my.js
@@ -1,8 +1,13 @@
-let age = prompt("Quel est votre âge ?");
-
-if(age > 18){
-  alert("Vous avez plus de 18 ans.");
+function surfaceRectagle(){
+  let largeur = prompt("Insérez la largeur (cm)");
+  let longueur = prompt("Insérez la longueur (cm)");
+  let surface = largeur * longueur;
+  alert("La surface est de : " + surface + " cm³");
 }
-else{
-  alert("Vous êtes mineur");
+
+function periRectagle(){
+  let largeur = prompt("Insérez la largeur (cm)");
+  let longueur = prompt("Insérez la longueur (cm)");
+  let perimetre = (largeur+longueur)*2;
+  alert("Le périmètre est de : " + perimetre + " cm²");
 }
