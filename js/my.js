8c9ddc2372afbafc77039869c3eb3bdb369aa36b
jonathancaron <jonathan.caron04@gmail.com>
2017-11-10 11:51:01 +0100

    base2 - ex6

--- js/my.js
+++ js/my.js
@@ -4,7 +4,17 @@ function additionne(x){
   return x + 2;
 }
 
+function soustrait(x){
+  let result = x - 2;
+  if(result >= 0){
+    return result;
+  }
+  else{
+    return "Nombre négatif!";
+  }
+}
+
 function affiche(){
-  alert(additionne(tab[0]));
-  alert(additionne(tab.length));
+  alert(soustrait(tab[0]));
+  alert(soustrait(tab.length));
 }
